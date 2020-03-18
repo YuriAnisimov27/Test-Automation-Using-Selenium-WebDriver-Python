2aac9b3f18871df530212cca74e1864a7dfb7413
YuriAnisimov27 <60671340+YuriAnisimov27@users.noreply.github.com>
2020-03-19 00:54:55 +0300

    Add files via upload

---
+++ lesson22step7.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+import os
+
+link = "http://suninjuly.github.io/file_input.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_css_selector("[name='firstname']")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector("[name='lastname']")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector("[name='email']")
+    input3.send_keys("1@2.3")
+
+    element = browser.find_element_by_css_selector("[type='file']")
+    current_dir = os.path.abspath(os.path.dirname(__file__))  # получаем путь к директории текущего исполняемого файла
+    file_path = os.path.join(current_dir, 'file.txt')  # добавляем к этому пути имя файла
+    element.send_keys(file_path)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(7)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
